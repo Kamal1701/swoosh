f9ddb7405d8bd136e55d9245d50fb7a473835602
Kamal1701 <kk17kannan@yahoo.com>
2023-07-06 10:49:09 +0530

    added setOnCheckedChangeListener for togglebutton on LeagueActivity.kt

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -2,10 +2,41 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.CompoundButton.OnCheckedChangeListener
+import com.example.swoosh.databinding.ActivityLeagueBinding
 
 class LeagueActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLeagueBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        binding = ActivityLeagueBinding.inflate(layoutInflater)
+        val viewLeague = binding.root
+//        setContentView(R.layout.activity_league)
+        setContentView(viewLeague)
+        binding.toggleBtnMens.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.toggleBtnMens.setBackgroundResource(R.drawable.swoosh_button_checked)
+            } else {
+                binding.toggleBtnMens.setBackgroundResource(R.drawable.swoosh_button)
+            }
+        }
+
+        binding.toggleBtnWomens.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.toggleBtnWomens.setBackgroundResource(R.drawable.swoosh_button_checked)
+            } else {
+                binding.toggleBtnWomens.setBackgroundResource(R.drawable.swoosh_button)
+            }
+        }
+
+        binding.toggleBtnCoEd.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.toggleBtnCoEd.setBackgroundResource(R.drawable.swoosh_button_checked)
+            } else {
+                binding.toggleBtnCoEd.setBackgroundResource(R.drawable.swoosh_button)
+            }
+        }
+
     }
 }
