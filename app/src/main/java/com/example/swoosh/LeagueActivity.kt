9789c57abec1bad256582695a01010fc3a8d02ae
Kamal1701 <kk17kannan@yahoo.com>
2023-07-06 14:32:07 +0530

    changed league activity xml and added SkillActivity layout

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,14 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.CompoundButton
 import android.widget.CompoundButton.OnCheckedChangeListener
 import com.example.swoosh.databinding.ActivityLeagueBinding
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     private lateinit var binding: ActivityLeagueBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,29 +16,35 @@ class LeagueActivity : AppCompatActivity() {
         val viewLeague = binding.root
 //        setContentView(R.layout.activity_league)
         setContentView(viewLeague)
-        binding.toggleBtnMens.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.toggleBtnMens.setBackgroundResource(R.drawable.swoosh_button_checked)
-            } else {
-                binding.toggleBtnMens.setBackgroundResource(R.drawable.swoosh_button)
-            }
-        }
 
-        binding.toggleBtnWomens.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.toggleBtnWomens.setBackgroundResource(R.drawable.swoosh_button_checked)
-            } else {
-                binding.toggleBtnWomens.setBackgroundResource(R.drawable.swoosh_button)
-            }
-        }
 
-        binding.toggleBtnCoEd.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.toggleBtnCoEd.setBackgroundResource(R.drawable.swoosh_button_checked)
-            } else {
-                binding.toggleBtnCoEd.setBackgroundResource(R.drawable.swoosh_button)
-            }
-        }
+//        binding.toggleBtnMens.setOnCheckedChangeListener { _, isChecked ->
+//            if (isChecked) {
+//                binding.toggleBtnMens.setBackgroundResource(R.drawable.swoosh_button_checked)
+//            } else {
+//                binding.toggleBtnMens.setBackgroundResource(R.drawable.swoosh_button)
+//            }
+//        }
+//
+//        binding.toggleBtnWomens.setOnCheckedChangeListener { _, isChecked ->
+//            if (isChecked) {
+//                binding.toggleBtnWomens.setBackgroundResource(R.drawable.swoosh_button_checked)
+//            } else {
+//                binding.toggleBtnWomens.setBackgroundResource(R.drawable.swoosh_button)
+//            }
+//        }
+//
+//        binding.toggleBtnCoEd.setOnCheckedChangeListener { _, isChecked ->
+//            if (isChecked) {
+//                binding.toggleBtnCoEd.setBackgroundResource(R.drawable.swoosh_button_checked)
+//            } else {
+//                binding.toggleBtnCoEd.setBackgroundResource(R.drawable.swoosh_button)
+//            }
+//        }
 
+    }
+    fun leagueNextClicked(view:View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
